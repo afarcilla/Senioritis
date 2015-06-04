28ce388ffc86aa58f572859900a5552ef6c901f1
nissayeva <natashaaay@gmail.com>
2015-06-04 13:42:12 -0700

    nothing important

--- app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/MainActivity.java
@@ -1,7 +1,8 @@
 package edu.ucsb.cs.cs185.afarcilla.senioritis;
 
+
 import android.content.SharedPreferences;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,7 +11,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
