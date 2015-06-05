fabf88e71dde1c5f2d84f9bae6061f2ba582c33b
Adrian Arcilla <afarcilla@gmail.com>
2015-06-05 16:57:42 -0700

    refactored some of the code

--- app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/TextEntryFragment.java
+++ app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/TextEntryFragment.java
@@ -1,18 +1,12 @@
 package edu.ucsb.cs.cs185.afarcilla.senioritis;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
-/**
- * Created by Natasha on 6/4/15.
- */
 public class TextEntryFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
