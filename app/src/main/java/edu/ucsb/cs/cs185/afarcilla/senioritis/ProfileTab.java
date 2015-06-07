cd5bc3931ce15f2ac65389332ff90b282623ab67
Adrian Arcilla <afarcilla@gmail.com>
2015-06-06 19:17:38 -0700

    changed the tab layouts for bucket list and profile

--- app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/ProfileTab.java
+++ app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/ProfileTab.java
@@ -1,11 +1,16 @@
 package edu.ucsb.cs.cs185.afarcilla.senioritis;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class ProfileTab extends Fragment {
 
@@ -17,9 +22,35 @@ public class ProfileTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_layout, container, false);
-        TextView tv = (TextView) v.findViewById(R.id.text);
-        tv.setText(this.getTag() + " Content");
+        View v = inflater.inflate(R.layout.profile_layout, container, false);
+        setDaysLeftView(v);
         return v;
+    }
+
+    public void setDaysLeftView(View v){
+
+        //get current date
+        Calendar c = Calendar.getInstance();
+        long current = (c.getTime()).getTime();
+
+        //get grad date
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        long gradDate = (preferences.getLong("gradDate", 0));
+
+        //calculate days left
+        long days = (gradDate - current) / (24 * 60 * 60 * 1000);
+        String daysLeft = String.valueOf(days);
+
+        //set textView
+        TextView text = (TextView) v.findViewById(R.id.daysleft);
+
+        if(gradDate == 0){daysLeft = "?";}
+        else if(days < 0 && gradDate != 0){
+            daysLeft= "ended";
+            text.setTextColor(getResources().getColor(R.color.light_pink));
+            text.setTypeface(null, Typeface.BOLD);
+        }
+
+        text.setText(daysLeft);
     }
 }
