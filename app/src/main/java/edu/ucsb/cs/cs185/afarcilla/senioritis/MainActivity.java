acd8a842eebefaba50be814e673b1e34287ca8ac
Adrian Arcilla <afarcilla@gmail.com>
2015-06-06 18:04:09 -0700

    Added tabs -- however, they are empty & they don't contain anything meaningful. Change set contentview in MainActivity to activity_main layout to set back to normal

--- app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/MainActivity.java
@@ -3,6 +3,8 @@ package edu.ucsb.cs.cs185.afarcilla.senioritis;
 import android.app.DialogFragment;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,14 +14,28 @@ import android.widget.TextView;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+    private FragmentTabHost mTabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test);
 
-        setDaysLeftView();
+        //setDaysLeftView();
+
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab1").setIndicator("Tab 1", null),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab2").setIndicator("Tab 2", null),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab3").setIndicator("Tab 3", null),
+                FragmentTab.class, null);
 
     }
 
