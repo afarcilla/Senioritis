bde2b9774a59e6ca887bf56a40034fc71021fbe0
Adrian Arcilla <afarcilla@gmail.com>
2015-06-06 18:52:22 -0700

    Implmented Tab fragments and added Home, Bucket List, and Profile tabs

---
+++ app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/HomeTab.java
@@ -0,0 +1,57 @@
+package edu.ucsb.cs.cs185.afarcilla.senioritis;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class HomeTab extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.home_layout, container, false);
+        setDaysLeftView(v);
+        return v;
+    }
+
+    public void setDaysLeftView(View v){
+
+        //get current date
+        Calendar c = Calendar.getInstance();
+        long current = (c.getTime()).getTime();
+
+        //get grad date
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        long gradDate = (preferences.getLong("gradDate", 0));
+
+        //calculate days left
+        long days = (gradDate - current) / (24 * 60 * 60 * 1000);
+        String daysLeft = String.valueOf(days);
+
+        //set textView
+        TextView text = (TextView) v.findViewById(R.id.daysleft);
+
+        if(gradDate == 0){daysLeft = "?";}
+        else if(days < 0 && gradDate != 0){
+            daysLeft= "ended";
+            text.setTextColor(getResources().getColor(R.color.light_pink));
+            text.setTypeface(null, Typeface.BOLD);
+        }
+
+        text.setText(daysLeft);
+    }
+}
--- app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/MainActivity.java
@@ -1,20 +1,15 @@
 package edu.ucsb.cs.cs185.afarcilla.senioritis;
 
 import android.app.DialogFragment;
-import android.content.SharedPreferences;
-import android.graphics.Typeface;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
-
-import java.util.Calendar;
 
 public class MainActivity extends FragmentActivity {
+
     private FragmentTabHost mTabHost;
 
     @Override
@@ -22,20 +17,20 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
 
-        //setDaysLeftView();
+
 
         mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
         mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
 
         mTabHost.addTab(
-                mTabHost.newTabSpec("tab1").setIndicator("Tab 1", null),
-                FragmentTab.class, null);
+                mTabHost.newTabSpec("Home").setIndicator("Home", null),
+                HomeTab.class, null);
         mTabHost.addTab(
-                mTabHost.newTabSpec("tab2").setIndicator("Tab 2", null),
-                FragmentTab.class, null);
+                mTabHost.newTabSpec("Bucket List").setIndicator("Bucket List", null),
+                BucketListTab.class, null);
         mTabHost.addTab(
-                mTabHost.newTabSpec("tab3").setIndicator("Tab 3", null),
-                FragmentTab.class, null);
+                mTabHost.newTabSpec("Profile").setIndicator("Profile", null),
+                ProfileTab.class, null);
 
     }
 
@@ -54,33 +49,6 @@ public class MainActivity extends FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void setDaysLeftView(){
-
-        //get current date
-        Calendar c = Calendar.getInstance();
-        long current = (c.getTime()).getTime();
-
-        //get grad date
-        SharedPreferences shared = getSharedPreferences(getString(R.string.preference_file_key),
-                MODE_PRIVATE);
-        long gradDate = (shared.getLong("gradDate", 0));
-
-        //calculate days left
-        long days = (gradDate - current) / (24 * 60 * 60 * 1000);
-        String daysLeft = String.valueOf(days);
-
-        //set textView
-        TextView text = (TextView) findViewById(R.id.daysleft);
-
-        if(gradDate == 0){daysLeft = "?";}
-        else if(days < 0 && gradDate != 0){
-            daysLeft= "ended";
-            text.setTextColor(getResources().getColor(R.color.light_pink));
-            text.setTypeface(null, Typeface.BOLD);
-        }
-
-        text.setText(daysLeft);
-    }
 
     public void addClass(View v) {
         DialogFragment newFragment = new TextEntryFragment();
