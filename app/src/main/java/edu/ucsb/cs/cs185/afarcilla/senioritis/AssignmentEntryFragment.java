e9ea45d16c8818fba33b317a51ee976674639271
nissayeva <natashaaay@gmail.com>
2015-06-09 14:02:15 -0700

    adding assignments continued

--- app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/AssignmentEntryFragment.java
+++ app/src/main/java/edu/ucsb/cs/cs185/afarcilla/senioritis/AssignmentEntryFragment.java
@@ -1,9 +1,46 @@
 package edu.ucsb.cs.cs185.afarcilla.senioritis;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 /**
  * Created by Natasha on 6/9/15.
  */
 public class AssignmentEntryFragment extends DialogFragment {
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LinearLayout layout = new LinearLayout(getActivity());
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        LayoutInflater i = LayoutInflater.from(getActivity());
+        builder.setView(i.inflate(R.layout.frag_assignment_info, null));
+
+        builder.setPositiveButton("Enter", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        return builder.create();
+    }
 }
